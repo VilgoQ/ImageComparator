d50ce343aea8004525260fd57fc93f97a53a9f56
Vladislav Sevryukov <vladislav.sevryukov@gmail.com>
2021-04-04 00:09:57 +0300

    +=build_comparison_table() and some refactoring

--- ImageComparator/ImageComparator/main.cpp
+++ ImageComparator/ImageComparator/main.cpp
@@ -2,12 +2,13 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
-#include <cassert>
-#include <filesystem>
 #include <string>
-#include <list>
+#include <vector>
+#include <map>
 
-void get_input_data(int& thr, std::list<std::string>& files)
+using table_t = std::map< std::pair< std::string, std::string >, size_t >;
+
+void get_input_data(int& thr, std::vector<std::string>& files)
 {
 	bool ok = false;
 	while (!ok)
@@ -31,10 +32,72 @@ void get_input_data(int& thr, std::list<std::string>& files)
 	}
 }
 
+size_t compare_images(const cv::Mat& first, const cv::Mat& second)
+{
+	return 0;
+}
+
+table_t build_comparison_table(const std::vector<std::string>& filenames)
+{
+	table_t compared_table;
+	size_t pos = 0;
+	if (filenames.size() > 2)
+	{
+		while (pos != filenames.size() - 1)
+		{
+			auto first_filename = filenames[pos];
+			cv::Mat first = cv::imread(first_filename);
+			if (first.empty())
+			{
+				std::cout << "Can't read image" << first_filename << std::endl;
+				continue;
+			}
+			for (size_t cur_pos = pos + 1; cur_pos != filenames.size(); ++ cur_pos)
+			{
+				auto second_filename = filenames[cur_pos];
+				cv::Mat second = cv::imread(second_filename);
+				if (second.empty())
+				{
+					std::cout << "Can't read image" << second_filename << std::endl;
+					continue;
+				}
+				else
+				{
+					size_t diff = compare_images(first, second);
+					compared_table.emplace(std::make_pair(first_filename, second_filename), diff);
+				}
+			}
+			++pos;
+		}
+		return compared_table;
+	}
+	else
+	{
+		auto first = filenames[0];
+		auto second = filenames[1];
+		cv::Mat first_img = cv::imread(first);
+		cv::Mat second_img = cv::imread(second);
+		if (first_img.empty() || second_img.empty())
+		{
+			std::cout << ("Can't read one of the images\n");
+			return compared_table;
+		}
+		size_t diff = compare_images(first_img, second_img);
+		compared_table.emplace(std::make_pair(first, second), diff);
+		return compared_table;
+	}
+}
+
 int main()
 {
 	int comparison_threshold = 0;
-	std::list<std::string> filenames;
+	std::vector<std::string> filenames;
 	get_input_data(comparison_threshold, filenames);
+	if (filenames.size() < 2)
+	{
+		std::cout << "Not enough images to compare\n";
+		return 0;
+	}
+	table_t table = build_comparison_table(filenames);
 	return 0;
 }
